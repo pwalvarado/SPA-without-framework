91f5d3de68eb57ca42e1b9902f29619ed3736077
Pedro Alvarado <pwalvarado@gmail.com>
2016-08-03 11:07:01 -0500

    Add createQueryHash and renderErrorPage functions

--- js/script.js
+++ js/script.js
@@ -224,12 +224,21 @@ $(function () {
     renderProductsPage(results);
   }
 
+  // Shows the error page.
   function renderErrorPage() {
-    // Shows the error page.
+    $('.error').addClass('visible');
   }
 
+  // Get the filters object, turn it into a string and write it into the hash.
   function createQueryHash(filters) {
-    // Get the filters object, turn it into a string and write it into the hash.
+    // Here we check if filters isn't empty.
+    if (!$.isEmptyObject(filters)) {
+      // Stringify the object via JSON.stringify and write it after the '#filter' keyword.
+      window.location.hash = '#filter/' + JSON.stringify(filters);
+    } else {
+      // If it's empty change the hash to '#' (the homepage).
+      window.location.hash = '#';
+    }
   }
 
 });
